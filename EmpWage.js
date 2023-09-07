d9c34401b9311a29c1cf7f740f993e3d8428c303
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-09-07 11:56:56 +0530

    [Prasanna] Add:implement the Attedence of the Employee

---
+++ EmpWage.js
@@ -0,0 +1,10 @@
+const IS_AbBSENT=0;
+let empcheck = Math.floor(Math.random()*10)%2;
+if(empcheck==IS_AbBSENT)
+{
+    console.log("Employee is absent");
+}
+else
+{
+console.log("Employee is Present");
+}
