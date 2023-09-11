e65dd1c52d468cf26b59bc0c7ec186177ae3c4ab
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-09-11 11:23:34 +0530

    [Prasannna] Add:implement the Array Functions in employee Wage Function To get particular Data

--- EmpWage.js
+++ EmpWage.js
@@ -81,11 +81,113 @@ const DailyWageArray = new Array();
 while (TotalEmphrs < MAX_HRS_IN_A_MONTH && totalWorkingDay < NUM_OF_WORKING_DAYS) {
     totalWorkingDay++;
     empCheck1=Math.floor(Math.random()*10)%3;
-    TotalEmphrs += getworkinghrs(empCheck1);
-    DailyWageArray.push(CalculateDailyWage(TotalEmphrs));
+    let emphr = getworkinghrs(empCheck1);
+    TotalEmphrs+=emphr;
+    DailyWageArray.push(CalculateDailyWage(emphr));
 }
 let empWage1 = CalculateDailyWage(TotalEmphrs);
 console.log("Total days :" + totalWorkingDay + " Total Hours : " + TotalEmphrs + " Emp Wages " + empWage1);
 console.log(DailyWageArray)
 
+//UC 7A
+/*var totempwage=0;
+const empDailyWageArray = new Array();
+function sum(dailywage)
+{
+    totempwage +=dailywage
+}
+DailyWageArray.forEach(sum);
+console.log("total Days :"+totalWorkingDay+" Total Hours : "+TotalEmphrs+" Emp Wages : "+totempwage);
+function totalwages(totalwage,dailywage)
+{
+    return totalwage+dailywage;
+}
+console.log(DailyWageArray.reduce(totalwages,0));
+//UC 7B
+let day=0;
+function DayWithWage(wage)
+{
+    day++;
+    console.log("Day no"+day+" Amount="+wage);
+}
+DailyWageArray.forEach(Element=>
+    (
+        DayWithWage(Element)
+    ));
 
+//UC 7C
+day=0;
+console.log("Days With Full Wage");
+function DayWithFullWage(wage)
+{
+    day++;
+    if(wage=="160")
+    {
+        console.log("Day No "+day+" Amount "+wage);
+    }
+}
+DailyWageArray.forEach(element => (
+    DayWithFullWage(element)
+));
+
+//UC 7D
+day=0;
+var count=0;
+function FullTimeWage(wage)
+{
+    day++;
+    if(wage == "160" && count == 0)
+    {
+        console.log("First Time When You Get Full Salary in Day "+day);
+        count++;
+    }
+}
+    DailyWageArray.forEach(element => (
+        FullTimeWage(element)
+    ));*/
+
+//UC 7A
+totalempwage=0;
+function Sum(dailywage){
+    totalempwage+=dailywage;
+}
+DailyWageArray.forEach(Sum);
+console.log(totalempwage);
+
+//b
+let daycount=0;
+function mapDayWithWage(dailywage){
+    daycount++;
+    return daycount+ " " +dailywage;
+}
+let mapwithDayArray=DailyWageArray.map(mapDayWithWage);
+console.log(DailyWageArray);
+
+//C
+function fullTimeWage(dailywage){
+    return dailywage.includes("160");
+}
+let fullTimeWageArr = mapwithDayArray.filter(fullTimeWage);
+console.log(fullTimeWageArr);
+
+//D
+console.log(mapwithDayArray.find(fullTimeWage));
+
+//E
+console.log(fullTimeWageArr.every(fullTimeWage));
+
+//F
+function partTimeWage(dailywage){
+    return dailywage.includes("80");
+}
+console.log(mapwithDayArray.some(partTimeWage));
+
+//G
+function totalDaysWorked(numOfDays, dailywage){
+    if(dailywage>0)
+    {
+        numOfDays++;
+    }
+    return numOfDays;
+}
+console.log(DailyWageArray.reduce(totalDaysWorked,0));
